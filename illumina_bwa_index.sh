6fe3fa64f6c12289c4d785b4f002cf758c0f79da
hrreeb <hreeb20@yahoo.com>
2024-12-17 17:21:23 -0600

    Add files via upload

---
+++ illumina_bwa_index.sh
@@ -0,0 +1,2 @@
+bwa index -p /scratch/biol726311/pseudomonas_gm41/mapping_to_assembly/hybrid_assembly 
+/scratch/biol726311//pseudomonas_gm41/Assembly/hybrid/contigs.fasta 
---
+++ pacbio_sam_view.sh
@@ -0,0 +1,2 @@
+samtools view -bS /scratch/biol726311/BIOL7263_Genomics/pseudomonas_gm41/mapping_to_assembly/pseud_pacbio.sam > /scratch/biol726311/BIOL7263_Genomics/pseudomonas_gm41/mapping_to_assembly/pseudo_pacbio.bam
+
